9c59024d7e2f4489f6b6175d23513196b680bbc2
Jacek Kowalski <evidexx@gmail.com>
2018-11-14 13:01:13 +0100

    Updates

--- precache-manifest.7e5939f683d1deed8d18ab0ebc646191.js
+++ precache-manifest.7e5939f683d1deed8d18ab0ebc646191.js
@@ -32,12 +32,12 @@ self.__precacheManifest = [
     "url": "/react-contact-app/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/react-contact-app/static/media/icons.faff9214.woff"
-  },
-  {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/react-contact-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react-contact-app/static/media/icons.faff9214.woff"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
@@ -48,36 +48,36 @@ self.__precacheManifest = [
     "url": "/react-contact-app/static/js/main.3bd2a683.chunk.js"
   },
   {
-    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
-    "url": "/react-contact-app/static/media/outline-icons.ad97afd3.ttf"
-  },
-  {
-    "revision": "e8c322de9658cbeb8a774b6624167c2c",
-    "url": "/react-contact-app/static/media/brand-icons.e8c322de.woff2"
-  },
-  {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/react-contact-app/static/media/outline-icons.ef60a4f6.woff"
-  },
-  {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/react-contact-app/static/media/brand-icons.13db00b7.eot"
-  },
-  {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/react-contact-app/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
     "url": "/react-contact-app/static/media/icons.962a1bf3.svg"
   },
   {
-    "revision": "a046592bac8f2fd96e994733faf3858c",
-    "url": "/react-contact-app/static/media/brand-icons.a046592b.woff"
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react-contact-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react-contact-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react-contact-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react-contact-app/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "/react-contact-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react-contact-app/static/media/brand-icons.a046592b.woff"
   },
   {
     "revision": "dae70f97b920a82b4c08",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-contact-app/precache-manifest.706cb48b7220a9fd85dc1c97b592a9fb.js"
+  "/react-contact-app/precache-manifest.7e5939f683d1deed8d18ab0ebc646191.js"
 );
 
 workbox.clientsClaim();
